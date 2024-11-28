99e354790db992480a558eb3b15e11bb83fab9d5
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2024-11-28 14:12:39 -0600

    Launches kitty with cmus, cava, and album art

---
+++ packages/kitty-music.nix
@@ -0,0 +1,15 @@
+{
+  pkgs,
+  my-kitty,
+  my-cava,
+  my-cmus,
+}: let
+  kitty-session = pkgs.writeText "session.conf" ''
+    layout splits
+    launch --var window=first ${my-cmus}/bin/cmus
+    launch --location=hsplit --bias 30 ${my-cava}/bin/cava
+    focus_matching_window var:window=first
+    launch --location=vsplit --bias 20 ${my-cmus}/bin/album-art.sh
+  '';
+in
+  pkgs.writeShellScriptBin "kitty-music" ''${my-kitty}/bin/kitty --session ${kitty-session}''
