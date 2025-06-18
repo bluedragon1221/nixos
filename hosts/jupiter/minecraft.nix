5672a0261ea9eec21f1c57b45b9d1b64d2d1f9cb
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2025-06-18 12:40:57 -0500

    more smb

--- hosts/jupiter/minecraft.nix
+++ hosts/jupiter/minecraft.nix
@@ -1,5 +1,7 @@
 {config, ...}: {
   networking.firewall.allowedUDPPorts = [19132];
+
+  virtualisation.oci-containers.backend = "podman";
   virtualisation.oci-containers.containers = {
     "Minecraft" = {
       environment = {
@@ -10,7 +12,6 @@
         TZ = config.time.timeZone;
         VERSION = "1.21.81.2";
         CONTENT_LOG_FILE_ENABLED = "true";
-        TEXTUREPACK_REQUIRED = "true";
 
         ALLOW_CHEATS = "false";
         DIFFICULTY = "1";
@@ -18,6 +19,8 @@
       image = "itzg/minecraft-bedrock-server";
       ports = ["0.0.0.0:19132:19132/udp"];
       volumes = ["/srv/minecraft/:/data"];
+
+      podman.sdnotify = "conmon"; # avoid nasty errors about healthcheck (idk, the service runs fine)
     };
   };
 }
--- hosts/jupiter/samba.nix
+++ hosts/jupiter/samba.nix
@@ -1,4 +1,17 @@
-{
+{config, ...}: {
+  users.users.${config.collinux.user.name}.extraGroups = ["brainusers"];
+  users.users.bob = {
+    isNormalUser = true;
+    extraGroups = ["brainusers"];
+    hashedPassword = "$y$j9T$L8CQv7V.LCD3gw/xR8M2T0$A4NHW9qpgc9pua7XE18P6ShICj.PNXHoyMW4yqmEMID";
+  };
+
+  users.groups."brainusers".name = "brainusers";
+
+  # sudo smbpasswd -a collin <PASSWORD>
+  # sudo smbpasswd -n bob # no password
+
+  networking.firewall.allowedTCPPorts = [445];
   services.samba = {
     enable = true;
     openFirewall = true;
@@ -6,13 +19,8 @@
       global = {
         "workgroup" = "WORKGROUP";
         "server string" = "brain2.0";
-
         "security" = "user";
-        "map to guest" = "bad user";
-        "guest account" = "nobody";
-
-        "bind interfaces only" = "no";
-        "interfaces" = "lo wlp2s0";
+        "map to guest" = "Bad User";
 
         # disable printing
         "load printers" = "no";
@@ -21,45 +29,35 @@
         "disable spoolss" = "yes";
       };
 
-      "brain2.0" = {
-        comment = "";
+      "brain2.0-public" = {
         path = "/srv/brain";
-        browseable = "yes";
-        "guest ok" = "yes";
         "read only" = "yes";
-        "create mask" = "0644";
-        "directory mask" = "0755";
-
-        "write list" = "";
-        "valid users" = "@users, guest";
-
-        # Hide guest-editable dirs
-        "veto files" = "/notes/worldbuilding/";
+        "guest ok" = "yes";
+        "force group" = "brainusers";
       };
 
       "brain2.0-admin" = {
-        comment = "";
         path = "/srv/brain";
+        "read only" = "no";
+        "valid users" = "collin";
+        writeable = "yes";
         browseable = "yes";
         "guest ok" = "no";
-        "valid users" = "collin";
-        "create mask" = "0644";
-        "directory mask" = "0755";
+        "create mask" = "0660";
+        "directory mask" = "0770";
+        "force group" = "brainusers";
       };
 
       "brain2.0-worldbuilding" = {
-        comment = "";
         path = "/srv/brain/notes/worldbuilding";
-        browseable = "yes";
-        "guest ok" = "yes";
         "read only" = "no";
-        "create mask" = "0664";
-        "directory mask" = "0775";
-        "force create mode" = "0664";
-        "force directory mode" = "0775";
-
-        "write list" = "@users, guest";
-        "valid users" = "@users, guest";
+        "guest ok" = "yes";
+        writeable = "yes";
+        browseable = "yes";
+        public = "yes"; # make windows not ask for password?
+        "create mask" = "0660";
+        "directory mask" = "0770";
+        "force group" = "brainusers";
 
         "delete readonly" = "no"; # prevent deletion of the directory
       };
