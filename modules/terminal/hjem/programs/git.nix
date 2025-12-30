5b68da7ee5216b35db966a1ae68ef38d2c5f91c2
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2025-12-30 10:47:50 -0600

    setup commit signing

--- modules/terminal/hjem/programs/git.nix
+++ modules/terminal/hjem/programs/git.nix
@@ -6,17 +6,25 @@
 }: let
   cfg = config.collinux.terminal.programs.git;
 
-  git_config = {
-    alias = {
-      stage = "add";
-      unstage = "restore --staged";
-    };
-    init.defaultBranch = "main";
-    user = {
-      email = cfg.userEmail;
-      name = cfg.userName;
-    };
-  };
+  git_config = lib.mkMerge [
+    {
+      alias = {
+        stage = "add";
+        unstage = "restore --staged";
+      };
+      init.defaultBranch = "main";
+      user = {
+        email = cfg.userEmail;
+        name = cfg.userName;
+      };
+    }
+    (lib.mkIf cfg.installKey {
+      # commit signing
+      gpg.format = "ssh";
+      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC3SjzIs3YI8PWJaNrAuaEeRcTcvIVHOKyCh2VwHTHEF";
+      commit.gpgsign = "true";
+    })
+  ];
 in {
   files = {
     ".config/git/config" = {
--- modules/terminal/nixos/github_ssh.nix
+++ modules/terminal/nixos/github_ssh.nix
@@ -13,14 +13,20 @@ in
       };
     };
 
+    hjem.users.${config.collinux.user.name}.files.".ssh/id_ed25519_github.pub".text = ''
+      ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC3SjzIs3YI8PWJaNrAuaEeRcTcvIVHOKyCh2VwHTHEF 96917990+bluedragon1221@users.noreply.github.com
+    '';
+
     programs.ssh = {
       knownHosts."github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+      startAgent = true;
 
       extraConfig = ''
         Host github.com
           User git
           IdentityFile ${config.collinux.secrets."github-ssh-key".path}
           IdentitiesOnly yes
+          AddKeysToAgent yes
       '';
     };
   }
