9979c0d6ff538354492e522d18aef22c6af64430
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2026-02-18 13:37:47 -0600

    clean up secrets config

--- secrets.nix
+++ secrets.nix
@@ -1,11 +1,10 @@
 let
   inherit ((builtins.fromTOML (builtins.readFile ./hosts.toml)).hosts) mercury ganymede jupiter;
 in {
-  "caddy-env.age".publicKeys = [ganymede.host_pubkey];
-  "tsnsrv-authkey.age".publicKeys = [ganymede.host_pubkey mercury.host_pubkey];
-  "williams-psk.age".publicKeys = [ganymede.host_pubkey];
-  "github-ssh-key.age".publicKeys = [mercury.host_pubkey jupiter.host_pubkey];
+  "hosts/ganymede/caddy-env.age".publicKeys = [ganymede.host_pubkey];
+  "hosts/ganymede/williams-psk.age".publicKeys = [ganymede.host_pubkey];
+  "modules/terminal/nixos/github-ssh-key.age".publicKeys = [mercury.host_pubkey jupiter.host_pubkey ganymede.host_pubkey];
 
-  "collin-copyparty-password.age".publicKeys = [mercury.host_pubkey ganymede.host_pubkey];
-  "collin-forgejo-password.age".publicKeys = [mercury.host_pubkey ganymede.host_pubkey];
+  "hosts/ganymede/collin-copyparty-password.age".publicKeys = [mercury.host_pubkey ganymede.host_pubkey];
+  "hosts/ganymede/collin-forgejo-password.age".publicKeys = [mercury.host_pubkey ganymede.host_pubkey];
 }
