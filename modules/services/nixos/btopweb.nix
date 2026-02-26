1932c82d72fb74971b180f84e06bcd1724e6271b
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2026-02-26 12:57:10 -0600

    a bit of customizing

--- modules/services/nixos/btopweb.nix
+++ modules/services/nixos/btopweb.nix
@@ -5,6 +5,15 @@
   ...
 }: let
   cfg = config.collinux.services.btopweb;
+
+  btopSettings = pkgs.writeText "btop.conf" ''
+    color_theme = "tomorrow-night"
+    enable_mouse = true
+    background_update = false
+
+    proc_tree = true
+    proc_colors = true
+  '';
 in {
   imports = [
     (import ./mkCaddyCfg.nix cfg)
@@ -28,7 +37,7 @@ in {
         User = "btopweb";
         Type = "simple";
 
-        ExecStart = ''${pkgs.ttyd}/bin/ttyd -W -i ${cfg.listenAddr} -p ${toString cfg.port} ${pkgs.btop}/bin/btop'';
+        ExecStart = ''${pkgs.ttyd}/bin/ttyd -W -i ${cfg.listenAddr} -p ${toString cfg.port} -t renderType=canvas -t fontSize=16 ${pkgs.btop}/bin/btop -c ${btopSettings}'';
       };
     };
   };
